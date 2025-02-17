d98f2045e50b32cf4a6856f39fedb75d16fd86b3
Jperez939 <perej939@newschool.edu>
2025-02-17 00:42:00 -0500

    february 16 updatesss

---
+++ script.js
@@ -0,0 +1,249 @@
+function stanza01() {
+    var x = document.getElementById("dm1-1");
+    if (x.innerHTML === "Life comes in wavelengths,") {
+      x.innerHTML = "Life is not constant, with moments of";
+    } else {
+      x.innerHTML = "Life comes in wavelengths,";
+    }
+
+    var x = document.getElementById("dm1-2");
+    if (x.innerHTML === "Different strips of sun,") {
+      x.innerHTML = "joy & despair causing lots of";
+    } else {
+      x.innerHTML = "Different strips of sun,";
+    }
+
+    var x = document.getElementById("dm1-3");
+    if (x.innerHTML === "Shades of each emotion,") {
+      x.innerHTML = "emotions that can be hard to";
+    } else {
+      x.innerHTML = "Shades of each emotion,";
+    }
+
+    var x = document.getElementById("dm1-4");
+    if (x.innerHTML === "Blinding when overdone.") {
+      x.innerHTML = "manage if excessive.";
+    } else {
+      x.innerHTML = "Blinding when overdone.";
+    }
+
+    var x = document.getElementById("stanza01lines");
+    if (x.innerHTML === "CLICK HERE FOR DOUBLE MEANING") {
+      x.innerHTML = "CLICK HERE FOR LINES 1-4";
+    } else {
+      x.innerHTML = "CLICK HERE FOR DOUBLE MEANING";
+    }
+
+    var x = document.getElementById("subtitle01-dm");
+    if (x.innerHTML === "LINES 1-4") {
+      x.innerHTML = "DOUBLE MEANING";
+    } else {
+      x.innerHTML = "LINES 1-4";
+    }
+}
+
+function stanza02() {
+    var x = document.getElementById("dm2-1");
+    if (x.innerHTML === "Amplitudes so deafening,") {
+      x.innerHTML = "This describes moments in life that";
+    } else {
+      x.innerHTML = "Amplitudes so deafening,";
+    }
+
+    var x = document.getElementById("dm2-2");
+    if (x.innerHTML === "A starburst of pure sound.") {
+      x.innerHTML = "are loud & intense, like excitement,";
+    } else {
+      x.innerHTML = "A starburst of pure sound.";
+    }
+
+    var x = document.getElementById("dm2-3");
+    if (x.innerHTML === "But then life can sidle up and whisper,") {
+      x.innerHTML = "passion, or chaos. But then a shift to";
+    } else {
+      x.innerHTML = "But then life can sidle up and whisper,";
+    }
+
+    var x = document.getElementById("dm2-4");
+    if (x.innerHTML === "What meaning have you found?") {
+      x.innerHTML = "a quieter, more reflective moment.";
+    } else {
+      x.innerHTML = "What meaning have you found?";
+    }
+
+    var x = document.getElementById("stanza02lines");
+    if (x.innerHTML === "CLICK HERE FOR DOUBLE MEANING") {
+      x.innerHTML = "CLICK HERE FOR LINES 5-8";
+    } else {
+      x.innerHTML = "CLICK HERE FOR DOUBLE MEANING";
+    }
+
+    var x = document.getElementById("subtitle02-dm");
+    if (x.innerHTML === "LINES 5-8") {
+      x.innerHTML = "DOUBLE MEANING";
+    } else {
+      x.innerHTML = "LINES 5-8";
+    }
+
+    var element = document.getElementById("subtitle02-dm");
+    element.classList.toggle("subtitle02-dm");
+
+    var element = document.getElementById("dm2-1");
+    element.classList.toggle("dm2-1");
+
+    var element = document.getElementById("dm2-2");
+    element.classList.toggle("dm2-2");
+
+    var element = document.getElementById("dm2-3");
+    element.classList.toggle("dm2-3");
+
+    var element = document.getElementById("dm2-4");
+    element.classList.toggle("dm2-4");
+}
+
+function stanza03() {
+    var x = document.getElementById("dm3-1");
+    if (x.innerHTML === "Colours to capture memories,") {
+      x.innerHTML = "Celebrate how we capture both big";
+    } else {
+      x.innerHTML = "Colours to capture memories,";
+    }
+
+    var x = document.getElementById("dm3-2");
+    if (x.innerHTML === "The frequencies of us all.") {
+      x.innerHTML = "& small moments in life that are";
+    } else {
+      x.innerHTML = "The frequencies of us all.";
+    }
+
+    var x = document.getElementById("dm3-3");
+    if (x.innerHTML === "There is no moment not made of light,") {
+      x.innerHTML = "meaningful, interconnected, & full of";
+    } else {
+      x.innerHTML = "There is no moment not made of light,";
+    }
+
+    var x = document.getElementById("dm3-4");
+    if (x.innerHTML === "The largest and the small.") {
+      x.innerHTML = "light no matter what form.";
+    } else {
+      x.innerHTML = "The largest and the small.";
+    }
+
+    var x = document.getElementById("stanza03lines");
+    if (x.innerHTML === "CLICK HERE FOR DOUBLE MEANING") {
+      x.innerHTML = "CLICK HERE FOR LINES 9-12";
+    } else {
+      x.innerHTML = "CLICK HERE FOR DOUBLE MEANING";
+    }
+
+    var x = document.getElementById("subtitle03-dm");
+    if (x.innerHTML === "LINES 9-12") {
+      x.innerHTML = "DOUBLE MEANING";
+    } else {
+      x.innerHTML = "LINES 9-12";
+    }
+}
+
+function stanza04() {
+    var x = document.getElementById("dm4-1");
+    if (x.innerHTML === "We each absorb wavelengths,") {
+      x.innerHTML = "We absorb energies from around us.";
+    } else {
+      x.innerHTML = "We each absorb wavelengths,";
+    }
+
+    var x = document.getElementById("dm4-2");
+    if (x.innerHTML === "Or reflect them back to the eye.") {
+      x.innerHTML = "Our interactions create something";
+    } else {
+      x.innerHTML = "Or reflect them back to the eye.";
+    }
+
+    var x = document.getElementById("dm4-3");
+    if (x.innerHTML === "Our interference constructive,") {
+      x.innerHTML = "beautiful & meaningful depending";
+    } else {
+      x.innerHTML = "Our interference constructive,";
+    }
+
+    var x = document.getElementById("dm4-4");
+    if (x.innerHTML === "As long as we choose to try.") {
+      x.innerHTML = "on our willingness to make the effort.";
+    } else {
+      x.innerHTML = "As long as we choose to try.";
+    }
+
+    var x = document.getElementById("stanza04lines");
+    if (x.innerHTML === "CLICK HERE FOR DOUBLE MEANING") {
+      x.innerHTML = "CLICK HERE FOR LINES 13-16";
+    } else {
+      x.innerHTML = "CLICK HERE FOR DOUBLE MEANING";
+    }
+
+    var x = document.getElementById("subtitle04-dm");
+    if (x.innerHTML === "LINES 13-16") {
+      x.innerHTML = "DOUBLE MEANING";
+    } else {
+      x.innerHTML = "LINES 13-16";
+    }
+
+    var element = document.getElementById("subtitle04-dm");
+    element.classList.toggle("subtitle04-dm");
+
+    var element = document.getElementById("dm4-1");
+    element.classList.toggle("dm4-1");
+
+    var element = document.getElementById("dm4-2");
+    element.classList.toggle("dm4-2");
+
+    var element = document.getElementById("dm4-3");
+    element.classList.toggle("dm4-3");
+
+    var element = document.getElementById("dm4-4");
+    element.classList.toggle("dm4-4");
+}
+
+function stanza05() {
+    var x = document.getElementById("dm5-1");
+    if (x.innerHTML === "But to see these strips of lightness,") {
+      x.innerHTML = "Maintain clarity & openness in order";
+    } else {
+      x.innerHTML = "But to see these strips of lightness,";
+    }
+
+    var x = document.getElementById("dm5-2");
+    if (x.innerHTML === "Our lenses must be clean.") {
+      x.innerHTML = "to perceive the beauty & meaning";
+    } else {
+      x.innerHTML = "Our lenses must be clean.";
+    }
+
+    var x = document.getElementById("dm5-3");
+    if (x.innerHTML === "So that when we gaze up at the stars,") {
+      x.innerHTML = "around you. Seeing clearly, allows us";
+    } else {
+      x.innerHTML = "So that when we gaze up at the stars,";
+    }
+
+    var x = document.getElementById("dm5-4");
+    if (x.innerHTML === "We’ll know that we too are seen.") {
+      x.innerHTML = "to understand that we matter.";
+    } else {
+      x.innerHTML = "We’ll know that we too are seen.";
+    }
+
+    var x = document.getElementById("stanza05lines");
+    if (x.innerHTML === "CLICK HERE FOR DOUBLE MEANING") {
+      x.innerHTML = "CLICK HERE FOR LINES 17-20";
+    } else {
+      x.innerHTML = "CLICK HERE FOR DOUBLE MEANING";
+    }
+
+    var x = document.getElementById("subtitle05-dm");
+    if (x.innerHTML === "LINES 17-20") {
+      x.innerHTML = "DOUBLE MEANING";
+    } else {
+      x.innerHTML = "LINES 17-20";
+    }
+}
